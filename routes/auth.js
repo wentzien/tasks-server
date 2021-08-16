8948ced9b3b157cd242e82f9727040b42055209d
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-16 23:40:01 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    const {error} = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    // const {error} = validate(req.body);
+    // if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({email: req.body.email});
     if (!user) return res.status(400).send("Invalid email or password.");
@@ -15,17 +15,17 @@ router.post("/", async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-    const token = user.generateAuthToken(user);
+    const token = user.generateAuthToken();
     res.send(token);
 });
 
-function validate(reqBody) {
-    const schema = Joi.object({
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    });
-
-    return schema.validate(reqBody);
-}
+// function validate(reqBody) {
+//     const schema = Joi.object({
+//         email: Joi.string().min(5).max(255).required().email(),
+//         password: Joi.string().min(5).max(255).required()
+//     });
+//
+//     return schema.validate(reqBody);
+// }
 
 module.exports = router; 
