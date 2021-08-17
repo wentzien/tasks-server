a0535e2bdf41a070840e1e002bf9f034faa3ae8e
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-17 22:28:10 +0200

    update task routes

--- models/Task.js
+++ models/Task.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const {Sequelize, DataTypes} = require("sequelize");
 const sequelize = require("../startup/db/sequelize").getORM();
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,81 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({mergeParams: true});
+const auth = require("../middleware/auth");
+const _ = require("lodash");
 const {User} = require("../models/User");
+const {Task, validateTask} = require("../models/Task");
 
-router.get("/", async (req, res) => {
-    const user = await User.create({name: "dennis", email:"dennis@wentzien.xyz", password: "123"});
-    res.send(user);
+router.get("/", [auth], async (req, res) => {
+    const user = await User.findByPk(req.user.id);
+    let tasklist = (await user.getTasklists({where: {id: req.params.tasklistId}}))[0]
+    if (!tasklist) return res.status(404).send("The tasklist with the given ID was not found.");
+
+    const tasks = tasklist.getTasks();
+
+    res.send(tasks);
+});
+
+router.post("/", [auth], async (req, res) => {
+    const {error} = validateTask(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findByPk(req.user.id);
+    let tasklist = (await user.getTasklists({where: {id: req.params.tasklistId}}))[0]
+    if (!tasklist) return res.status(404).send("The tasklist with the given ID was not found.");
+
+    const task = await Task.create({
+        description: req.body.description
+    });
+
+    await tasklist.addTask(task);
+
+    res.send(task);
+});
+
+router.put("/:id", [auth], async (req, res) => {
+    const {error} = validateTask(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findByPk(req.user.id);
+    let tasklist = (await user.getTasklists({where: {id: req.params.tasklistId}}))[0]
+    if (!tasklist) return res.status(404).send("The tasklist with the given ID was not found.");
+
+    let task = (await tasklist.getTasks({where: {id: req.params.id}}))[0];
+    if (!task) return res.status(404).send("The task with the given ID was not found.");
+
+    task.description = req.body.description;
+
+    await task.save();
+
+    task = _.pick(task, ["description"]);
+
+    res.send(task);
+});
+
+router.delete("/:id", [auth], async (req, res) => {
+    const user = await User.findByPk(req.user.id);
+    let tasklist = (await user.getTasklists({where: {id: req.params.tasklistId}}))[0]
+    if (!tasklist) return res.status(404).send("The tasklist with the given ID was not found.");
+
+    let task = (await tasklist.getTasks({where: {id: req.params.id}}))[0];
+    if (!task) return res.status(404).send("The task with the given ID was not found.");
+
+    task.destroy();
+
+    task = _.pick(task, ["description"]);
+
+    res.send(task);
+});
+
+router.get("/:id", [auth], async (req, res) => {
+    const user = await User.findByPk(req.user.id);
+    let tasklist = (await user.getTasklists({where: {id: req.params.tasklistId}}))[0]
+    if (!tasklist) return res.status(404).send("The tasklist with the given ID was not found.");
+
+    let task = (await tasklist.getTasks({where: {id: req.params.id}}))[0];
+    if (!task) return res.status(404).send("The task with the given ID was not found.");
+
+    res.send(task);
 });
 
 module.exports = router;
