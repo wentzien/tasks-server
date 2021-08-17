9abf23c6260bd10b9a8b645325446f184090ad06
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-17 10:06:36 +0200

    fix auth bug

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
     const {error} = validateAuth(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({email: req.body.email});
+    let user = await User.findOne({where: {email: req.body.email}});
     if (!user) return res.status(400).send("Invalid email or password.");
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
