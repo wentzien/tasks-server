dfed3f79008be25cb428744231e8276564bf60a5
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-29 22:16:19 +0200

    get user invites and accept/decline invites

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const {User, validateUser} = require("../models/User");
+const {Share} = require("../models/Share");
 const express = require("express");
 const router = express.Router();
 
@@ -31,6 +32,47 @@ router.post("/", async (req, res) => {
         .header("x-auth-token", token)
         .header("access-control-expose-headers", "x-auth-token")
         .send(token);
+})
+
+// -----------------------------------------------------------
+// invites
+
+router.get("/invites", [auth], async (req, res) => {
+    const userInvites = await User.findOne({
+        where: {id: req.user.id},
+        include: {
+            model: Share,
+            as: "InvitedUser",
+            where: {
+                status: "Invited"
+            }
+        }
+    });
+
+    const invites = userInvites.InvitedUser;
+
+    res.send(invites);
+});
+
+router.get("/invites/:sharedId/:status", [auth], async (req, res) => {
+    if (req.params.status !== "accept" && req.params.status !== "decline") return res.status(404).send("Invite status cannot be changed.");
+    const invite = await Share.findOne({
+        where: {
+            id: req.params.sharedId,
+            status: "Invited",
+            InvitedUserId: req.user.id
+        }
+    });
+    if (!invite) return res.status(404).send("Invite was not found or is not longer active.");
+
+    invite.status = req.params.status === "accept" ? "Accepted" : "Declined";
+    await invite.save();
+
+    res.send(invite);
+});
+
+router.get("/invites/:tasklistId/decline", [auth], async (req, res) => {
+
 });
 
 module.exports = router;
